4f6d116d8f94d7f53de4adea0f1bdef0ab3df5bc
Waris <oloyedewaris@gmail.com>
2023-02-18 03:28:26 +0100

    implemented react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,19 @@ import type { AppProps } from 'next/app';
 import { Box, ChakraProvider } from '@chakra-ui/react';
 import customTheme from '../utils/theme';
 import Navbar from '../components/Navbar';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient()
   return (
-    <ChakraProvider resetCSS theme={customTheme}>
-      <Box maxW="100vw" minH="100vh" bg="background" color="white">
-        <Navbar />
-        <Component {...pageProps} />
-      </Box>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider resetCSS theme={customTheme}>
+        <Box maxW="100vw" minH="100vh" bg="background" color="white">
+          <Navbar />
+          <Component {...pageProps} />
+        </Box>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,67 @@
 import React, { useEffect, useState } from "react"
-import { SimpleGrid, VStack, Button } from "@chakra-ui/react"
+import { SimpleGrid, VStack, CircularProgress, Button } from "@chakra-ui/react"
 import MovieCard from '../components/MovieCard';
 import axios from "axios";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 const Home = () => {
-  const [movies, setMovies] = useState([]);
-  const [loaded, setLoaded] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
 
-  useEffect(() => {
-    loadMovies()
-  }, [])
+  const firstQuery = useQuery({
+    queryKey: ['movie'],
+    queryFn: () => axios
+      .get(`https://www.omdbapi.com/?i=tt0848228&apikey=cadc1a65&s=Avengers&page=${page}`)
+      .then(res => res.data.Search)
+  })
 
-  const loadMovies = () => {
-    setLoading(true)
-    axios
-      .get(`https://www.omdbapi.com/?i=tt0848228&apikey=cadc1a65&s=Avengers&page=1`)
+  const loadMoreMutation = useMutation({
+    mutationFn: () => axios
+      .get(`https://www.omdbapi.com/?i=tt0848228&apikey=cadc1a65&s=Avengers&page=${page + 1}`)
       .then(res => {
-        setLoading(false)
-        setLoaded(true)
-        setMovies(res.data.Search)
+        setPage(page + 1)
+        return res.data.Search
       })
-      .catch(error => {
-        setLoading(false)
-      })
+  })
+
+  const handleLoadMore = () => {
+    loadMoreMutation.mutate()
   }
 
   return (
     <VStack py={8} w="100%">
-      <SimpleGrid columns={{ sm: 2, md: 3, lg: 4, xl: 5 }} gap={5}>
-        {movies?.map(movie => (
-          <MovieCard movie={movie} />
-        ))}
-      </SimpleGrid>
-      {loaded && <Button bg='#7b51ba' mx='auto' size='md'>Load More</Button>}
-    </VStack>
+      {firstQuery.isLoading ? (
+        <CircularProgress isIndeterminate value={30} size='120px' />
+      ) : firstQuery.isError ? (
+        <div>An error occurred</div>
+      ) : (
+        <SimpleGrid columns={{ sm: 2, md: 3, lg: 4, xl: 5 }} gap={5}>
+          {loadMoreMutation?.data ? (
+            <>
+              {[...firstQuery.data, ...loadMoreMutation.data]?.map(movie => (
+                <MovieCard key={movie.Title} movie={movie} />
+              ))}
+            </>
+          ) : (
+            <>
+              {[...firstQuery.data]?.map(movie => (
+                <MovieCard key={movie.Title} movie={movie} />
+              ))}
+            </>
+          )}
+        </SimpleGrid>
+      )}
+
+      {!firstQuery.isLoading && (
+        <Button
+          isLoading={loadMoreMutation.isLoading}
+          disabled={loadMoreMutation.isLoading}
+          loadingText='Loading...'
+          onClick={handleLoadMore}
+          bg='#7b51ba' mx='auto' size='md'>
+          Load more
+        </Button>
+      )}
+    </VStack >
   )
 }
 
